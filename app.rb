534e87f3a59252a35aa294ffd7af8b91358519da
Edward Weng <edweng@gmail.com>
2016-05-28 14:06:50 -0400

    Adding first version of working database server.

--- app.rb
+++ app.rb
@@ -1,1 +1,16 @@
 require 'sinatra'
+
+database = {}
+
+get '/set' do
+  database = database.merge(params)
+  [200, params.map { |k, v| "#{k} was set to #{v}\n" }]
+end
+
+get '/get' do
+  begin
+    database.fetch(params[:key])
+  rescue KeyError
+    [404, ["Key not found"]]
+  end
+end
